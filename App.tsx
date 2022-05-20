9e69b6e6a103ccd76957592daba0bdc576cfa5d7
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-05-19 22:46:54 -0300

    add linear gradient

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
 
-import AppLoading from "expo-app-loading";
-import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
-import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
-import { ThemeProvider } from "styled-components/native";
+import AppLoading from 'expo-app-loading';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
 
-import theme from "./src/theme";
+import theme from './src/theme';
+
+import { SignIn } from './src/pages/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts([
@@ -18,5 +19,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
 }
